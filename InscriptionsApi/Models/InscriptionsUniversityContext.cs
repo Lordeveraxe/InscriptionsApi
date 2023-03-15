e05efcdcf2fa2a5e7c5397a0802fa854e8fdd2ee
Andres Torres <santiago.ojedatorrres@gmail.com>
2023-03-15 17:56:27 -0500

    Eliminacion de metodo conexion a la base de datos, estatico

--- InscriptionsApi/Models/InscriptionsUniversityContext.cs
+++ InscriptionsApi/Models/InscriptionsUniversityContext.cs
@@ -21,10 +21,6 @@ public partial class InscriptionsUniversityContext : DbContext
 
     public virtual DbSet<Subject> Subjects { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=PREDATOR300\\SQLEXPRESS;Initial Catalog=Inscriptions_university;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Inscription>(entity =>
